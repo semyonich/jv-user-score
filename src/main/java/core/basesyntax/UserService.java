c2cf6d895caffbbca6dd9bb84d9a422831f178af
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-03 13:54:17 +0200

    all tests have passed!

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
-    public int getUserScore(String[] records, String email) {
-        return 0;
+    public int getUserScore(String[] records, String email) throws UserNotFoundException {
+        for (String record : records) {
+            String[] parts = record.split(":");
+            if (parts[0].equals(email)) {
+                return Integer.parseInt(parts[1]);
+            }
+        }
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
